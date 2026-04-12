8ae149323bd58c36a3a7e947edef29643fa82b02
paulveloz28 <paulveloz28@gmail.com>
2026-04-12 17:13:33 -0500

    agregar bajar limon

--- limones.js
+++ limones.js
@@ -4,11 +4,18 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
-let personajeX=canvas.width/2;
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
+
+
+let personajeX=canvas.width/2
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 function dibujarSuelo(){
     ctx.fillStyle="blue"; 
@@ -18,10 +25,16 @@ function dibujarSuelo(){
 function dibujarPersonaje(){
     ctx.fillStyle="yellow"; 
     ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
+    actualizarPantalla();
+}
+//moverderecha
+function moverDerecha(){
+    personajeX=personajeX+10;
     actualizarPantalla();
 }
 
@@ -29,8 +42,19 @@ function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="green"; 
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+}
+
+function bajarLimon(){
+    limonY=limonY+10;
+    actualizarPantalla();
 }
